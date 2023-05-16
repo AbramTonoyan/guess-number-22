5b83724bdadaeed8625b22318db4bceee317f4a4
Abram Tonoyan <abo.abram.tonoyan@gmail.com>
2023-05-16 21:50:30 +0400

    adding final logic inplemented

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+let highScore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, 'GUESS');
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    console.log(score, '**********************');
+    document.querySelector('.score').textContent = score;
+    if (score > 0) {
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'too HIgh!' : 'too Low!';
+      score--;
+      Document.querySelector('.score').textContent = score;
+    } else {
+      console.log('you lost');
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  let score = 20;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+});
+
+console.log(secretNumber);
